c819044750538ef1b3978a3844e4087b07e5fa75
Avi Kadria <avi.kadria3@gmail.com>
2020-04-06 06:31:06 -0700

    removed the old compile script

--- my_boot/compile.sh
+++
@@ -1,5 +0,0 @@
-as --32 -o boot.o boot.s
-ld -m elf_i386 boot.o --oformat binary -o boot.bin  -Ttext 0x7c00
-dd status=noxfer conv=notrunc if=boot.bin of=boot.flp
-qemu-system-i386 boot.flp 
-rm boot.o boot.flp
